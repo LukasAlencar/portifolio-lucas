7b692217699c07fb0d0991dbe2c6d2b0b19f82b7
lukasalencar <lucas.kauan@aluno.ifsp.edu.br>
2023-12-12 17:22:40 -0300

    fixed bug

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,10 +1,11 @@
-import { ProjectDetails } from "@/app/components/pages/project/project-details";
-import { ProjectSections } from "@/app/components/pages/project/project-sections";
-import { ProjectPageData, ProjectsPageStaticData } from "@/app/types/page-info";
-import { fetchHygraphQuery } from "@/app/utils/fetch-hygraph-query";
-import { Metadata } from "next";
+import { ProjectDetails } from '@/app/components/pages/project/project-details'
+import { ProjectSections } from '@/app/components/pages/project/project-sections'
+import { ProjectPageData, ProjectsPageStaticData } from '@/app/types/page-info'
+import { fetchHygraphQuery } from '@/app/utils/fetch-hygraph-query'
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
-type ProjectsProps = {
+type ProjectProps = {
   params: {
     slug: string
   }
@@ -13,43 +14,45 @@ type ProjectsProps = {
 const getProjectDetails = async (slug: string): Promise<ProjectPageData> => {
   const query = `
   query ProjectQuery() {
-      project(where: {slug: "${slug}"}) {
-        pageThumbnail {
-          url
-        }
-        thumbnail {
-          url
-        }
-        sections {
-          title
-          image {
-            url
-          }
-        }
-        title
-        shortDescription
-        description {
-          raw
-          text
-        }
-        technologies {
-          name
-        }
-        liveProjectUrl
-        githubUrl
+    project(where: {slug: "${slug}"}) {
+      pageThumbnail {
+        url
       }
+      thumbnail {
+        url
+      }
+      sections {
+        title
+        image {
+          url
+        }
+      }
+      title
+      shortDescription
+      description {
+        raw
+        text
+      }
+      technologies {
+        name
+      }
+      liveProjectUrl
+      githubUrl
     }
+  }
   `
-
-  return fetchHygraphQuery(
+  const data = fetchHygraphQuery<ProjectPageData>(
     query,
-    60 * 60 * 24
+    1000 * 60 * 60 * 24, // 1 day
   )
+
+  return data
 }
 
-export default async function Project({ params: { slug } }: ProjectsProps) {
-
+export default async function Project({ params: { slug } }: ProjectProps) {
   const { project } = await getProjectDetails(slug)
+
+  if (!project?.title) return notFound()
 
   return (
     <>
@@ -62,17 +65,19 @@ export default async function Project({ params: { slug } }: ProjectsProps) {
 export async function generateStaticParams() {
   const query = `
     query ProjectsSlugsQuery() {
-      projects(first: 100){
+      projects(first: 100) {
         slug
       }
     }
   `
-  const { projects } = await fetchHygraphQuery<ProjectsPageStaticData>(query);
+  const { projects } = await fetchHygraphQuery<ProjectsPageStaticData>(query)
 
   return projects
 }
 
-export async function generateMetadata({ params: { slug } }: ProjectsProps): Promise<Metadata> {
+export async function generateMetadata({
+  params: { slug },
+}: ProjectProps): Promise<Metadata> {
   const data = await getProjectDetails(slug)
   const project = data.project
 
@@ -85,8 +90,8 @@ export async function generateMetadata({ params: { slug } }: ProjectsProps): Pro
           url: project.thumbnail.url,
           width: 1200,
           height: 630,
-        }
-      ]
-    }
+        },
+      ],
+    },
   }
 }
